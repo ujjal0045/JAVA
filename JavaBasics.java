98f3c7eb7957c5074e384a226e512f435f53a4ad
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-08-01 18:15:22 +0530

    BoilerPlate COde and start learning Java Language

---
+++ JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics {
+  public static void main(String args[]){
+
+    System.out.println("*****");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+
+  }
+}
+// boilerplate COde
