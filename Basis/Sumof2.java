0bba1b3dc1951d45be871963bd45e9dbefb7f32c
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-02 21:40:24 +0530

    Doing sum of 2 number using input by user

--- Basis/Sumof2.java
+++ Basis/Sumof2.java
@@ -1,10 +1,19 @@
 package Basis;
 
+import java.util.Scanner;
+
 public class Sumof2 {
   public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
+    // int a = 10;
+    // int b = 5;
+    // int sum = a + b;
+    // System.out.println(sum);
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter your first Number: ");
+    int a = sc.nextInt();
+    System.out.print("Enter your second Number: ");
+    int b = sc.nextInt();
     int sum = a + b;
-    System.out.println(sum);
+    System.out.println("Sum: "+sum);
   }
 }
