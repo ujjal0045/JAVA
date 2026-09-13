c3a26211d156943921fadb9838bd2dfc57f35005
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-13 19:15:29 +0530

    Add SumOfN.java to calculate the sum of numbers up to a given range

---
+++ Loops/SumOfN.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class SumOfN {
+  public static void main(String[] args) {
+    Scanner sc=  new Scanner(System.in);
+    System.out.print("Enter the range :");
+    int n = sc.nextInt();
+    int i =1,sum=0;
+    while(i<=n){
+      sum += i++;
+    }
+    System.out.println("sum of n number: "+ sum);
+    sc.close();
+
+  }
+}
