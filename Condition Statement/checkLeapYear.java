3427f421a8bda0ad644fcefdf3d1b80cbd3eb122
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-13 18:48:26 +0530

    Add checkLeapYear.java to determine if a year is a leap year based on user input

---
+++ Condition Statement/checkLeapYear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class checkLeapYear {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+    if(
+      (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    ){
+      System.out.println("Leap year");
+    } else{
+      System.out.print("Not an leap year ");
+    }
+  }
+}
