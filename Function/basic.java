b781cc2045c04cdc04cf4eac0b15be0eaf507a1f
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-17 19:58:27 +0530

    Add basic.java to implement a simple Hello World program

---
+++ Function/basic.java
@@ -0,0 +1,10 @@
+package Function;
+
+public class basic {
+  public static void printHelloWorld() {
+    System.out.println("Hello World");
+  }
+  public static void main(String[] args) {
+    printHelloWorld(); 
+  }
+}
