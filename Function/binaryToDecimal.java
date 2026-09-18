dc80e3fa102f3454cffc77dab5a96f5feb4df239
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-18 16:56:55 +0530

    Refactor binaryToDecimal.java to use conversion2 method for binary to decimal conversion

--- Function/binaryToDecimal.java
+++ Function/binaryToDecimal.java
@@ -15,9 +15,23 @@ public class binaryToDecimal {
     }
     return sum;
   }
+  // better approach 
+  public static int conversion2(int n){
+    
+    int sum = 0, p = 0;
+    while(n>0){
+      int r = n % 10;
+      if(r == 1){
+        sum += Math.pow(2, p);
+      }
+      p++;
+      n /= 10;
+    }
+    return sum;
+  }
   public static void main(String[] args) {
     int n = 1111;
-    int n1 = conversion(n);
+    int n1 = conversion2(n);
     System.out.println(n1);
   }
 }
