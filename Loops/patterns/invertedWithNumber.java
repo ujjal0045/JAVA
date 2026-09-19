cae122872b4538f394fb712eb506d2cd5b599eca
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-19 20:54:33 +0530

    Fix loop condition in invertedWithNumber.java to correctly print the inverted number triangle pattern

--- Loops/patterns/invertedWithNumber.java
+++ Loops/patterns/invertedWithNumber.java
@@ -3,7 +3,7 @@ package Loops.patterns;
 public class invertedWithNumber {
   public static void pattern(){
     for(int i=1;i<=5;i++){
-      for(int j=1;j<=6-i;j++){
+      for(int j=1;j<=5-i+1;j++){
         System.out.print(j);
       }
       System.out.println();
