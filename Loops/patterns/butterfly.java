8adda2aa4287ca43cbf0396ce455e3376d38ac22
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-19 23:26:00 +0530

    Add butterfly.java to implement a program for printing a butterfly pattern

---
+++ Loops/patterns/butterfly.java
@@ -0,0 +1,33 @@
+package Loops.patterns;
+
+public class butterfly {
+  public static void pattern(int n) {
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      for(int k=1;k<= 2*(n-i);k++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      for(int k=1;k<= 2 * (n-i);k++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    pattern(4); 
+  }
+}
